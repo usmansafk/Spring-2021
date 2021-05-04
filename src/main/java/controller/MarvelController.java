20256b22bf2a7ca394e139cb898a5820b0ab10c9
Usman Sajid <usman230@hotmail.co.uk>
2021-05-04 12:57:23 +0100

    updated marvel controller

--- src/main/java/controller/MarvelController.java
+++ src/main/java/controller/MarvelController.java
@@ -3,8 +3,11 @@ package controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,12 +28,28 @@ public class MarvelController  {
 	}
 	
 	//READ
-	@GetMapping("/getMarvel")
+	@GetMapping("/getAlll")
 	public List<Marvel> getMarvel(){
 		return this.marvelList;
 	}
 	
+	//READ ONE
+	@GetMapping("/getOne/{index}")
+	public Marvel getCharacterById(@PathVariable int index) {
+		return this.marvelList.get(index);
+	}
 	
+	//DELETE
+	@DeleteMapping("/remove/{index}")
+	public Marvel removeCharacter(@PathVariable int index) {
+		return this.marvelList.get(index);
+	}
+	
+	//UPDATE
+	@PutMapping("/update{index}")
+	public Marvel updateCharacter(@PathVariable int index) {
+		return this.marvelList.get(index);
+	}
 	
 
 }
